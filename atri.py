175c7982209b0924d7a69e037f24738618b6fc9c
Fovlin <fovlin@163.com>
2026-04-24 15:31:39 +0800

    Update atri.py

--- atri.py
+++ atri.py
@@ -4,53 +4,64 @@ import time
 import socket
 from openai import OpenAI
 
-LOG_PATH = "./logs/latest.log" # 你的服务器日志路径
+LATEST_LOG_PATH = ".logs/latest.log" # 你的服务器日志路径
 API_KEY = "" # 你的 API 密钥
-HOST = "127.0.0.1"
-PASSWD = "" # 你的密码
+PASSWD = "" # 你的 RCON 密码
 BASE_URL = "https://api.deepseek.com" # 根据实际情况替换，如果换成 deepseek 以外的模型，手动修改 48 行
 
 sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def sock_start():
+# 发送登录数据包的方法
+def socket_start():
     sc.connect(('localhost',25575))
     passwd = PASSWD.encode('utf-8')
     len_pack = (len(passwd) + 11).to_bytes(4,byteorder="little",signed=True)
     sc.send(len_pack + b"\x01\x00\x00\x00" + b"\x03\x00\x00\x00" + passwd + b"\x00\x00\x00")
     sc.recv(1024)
 
+# 发送指令数据包的方法
 def socket_send(command):
     command = command.encode('utf-8')
     bytes_len = (len(command) + 11).to_bytes(4,byteorder="little",signed=True)
     sc.send(bytes_len + b"\x02\x00\x00\x00" + b"\x02\x00\x00\x00" + command + b"\x00\x00\x00")
     sc.recv(1024)
 
-sock_start()
 
-old_log="x"
+# 读取并写入特定长度的日志的方法
+def read_write_chat_log(UserMessage):
+    with open(LATEST_LOG_PATH,'r') as file:
+        all_log = str(file.readlines()[-32:]).strip('[\n')
+    context[1] = {"role": "system", "content": "最近的服务器日志：" + all_log}
+    data1={"role": "user", "content":UserMessage} # 写入用户消息
+    context.append(data1)
+
+socket_start()
+
+old_last_line="x"
 
 while True:
 
     # 读取最后一行
-    with open(LOG_PATH,'r',encoding='utf-8') as file:
-        log = file.readlines()[-1].strip("\n")
-        key = log[-3:]
+    with open(LATEST_LOG_PATH,'r') as file:
+        last_line = None
+        for line in file:
+            last_line = line.strip()
+        key = last_line[-3:]
 
     if key == ">>>":
 
-        if log != old_log:
+        if last_line != old_last_line:
+
+            print(last_line)
 
             with open('chat.json','r') as file:
                 context = json.load(file)
 
-            UserMessage=log
+            old_last_line = last_line # 将本次的日志传递给锁，防止同日志触发机器人回复
+            
+            read_write_chat_log(UserMessage=last_line)
 
-            old_log=log
-        
-            data1={"role": "user", "content":UserMessage}
-
-            context.append(data1)
-
+            # openai 模块
             client = OpenAI(
                 api_key=API_KEY,
                 base_url=BASE_URL)
@@ -63,22 +74,21 @@ while True:
                 max_tokens=5120
             )
 
+            # 写入 ai 回复消息
             data2 = {"role": "assistant", "content": response.choices[0].message.content}
-
             context.append(data2)
 
-            chat_len=len(context)
-
             # 控制 json 文件长度，防止我的 token 爆炸，你可以自定义长度。
-
+            chat_len=len(context)
             if chat_len >= 64:
-                del context[1:5]
+                del context[2:6]
 
+            # 将对话列表重新写入 json 文件
             with open('chat.json','w') as file:
-                json.dump(context,file,ensure_ascii=False,indent=2)
+                json.dump(context,file,ensure_ascii=False,indent=4)
 
             TEXT = response.choices[0].message.content
-
+            print("[ATRI] " + TEXT)
             socket_send(command="tellraw @a [{text:\"[ATRI] \",color:\"aqua\"},{text:\"" + TEXT + "\",color:\"white\"}]")
 
         else:
@@ -86,9 +96,8 @@ while True:
     else:
         time.sleep(3)
 
-# =====================================================
-#             一时兴起自学 Python 一天的成果
-#  bug 满天飞，欢迎各位大佬随时批评指正（打字带括号是在调用函数吗
+# ===================================================#
+#          bug 满天飞，欢迎各位大佬随时批评指正!          
 #
 #                       _oo0oo_
 #                      o8888888o
@@ -106,8 +115,8 @@ while True:
 #          ."" '<  `.___\_<|>_/___.'_> '".
 #         | | :  `- \`. ;`. _/ ; .-' / : | |
 #         \  \ `-.   \_ __\ /__ _/   .-' /  /
-#  ======`-.____`-.___\_____/___.-`____.-'============
+#  ======`-.____`-.___\_____/___.-`____.-'==========
 #                       `=---='
 #
 #          佛祖保佑        无bug、不报错
-# ====================================================
+# ===================================================
